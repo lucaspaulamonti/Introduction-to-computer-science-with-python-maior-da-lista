324c35c8a864b5e6fcd0ef17490f593c5c923e8a
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 13:49:46 -0300

    Create maior-da-lista.py

---
+++ maior-da-lista.py
@@ -0,0 +1,5 @@
+# Escreva a função maior_elemento que recebe como parâmetro uma lista com números inteiros e devolve um número inteiro correspondente ao maior valor.
+def maior_elemento(list):
+    return max(list)
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
